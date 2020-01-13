8699f28716c3ae2a7897199eda152d578b7b6b23
yoshisman8 <4214215+yoshisman8@users.noreply.github.com>
2020-01-13 18:03:49 -0400

    API implementation is a go

--- NethysBot/Models/Character.cs
+++ NethysBot/Models/Character.cs
@@ -7,10 +7,38 @@ namespace NethysBot.Models
 {
 	class Character
 	{
+		/// <summary>
+		/// Internal ID.
+		/// </summary>
 		[BsonId]
 		public int Id { get; set; }
+
+		/// <summary>
+		/// id of whoever imported the character.
+		/// </summary>
+		public ulong Owner { get; set; }
+
+		/// <summary>
+		/// ID on characters.pf2.tools.
+		/// </summary>
 		public string RemoteId { get; set; }
+
+		/// <summary>
+		/// Character Name for easy querying.
+		/// </summary>
 		public string Name { get; set; }
-		public string Cache { get; set; }
+
+		/// <summary>
+		/// Backup of the complete sheet.
+		/// </summary>
+		public string SheetCache { get; set; }
+		/// <summary>
+		/// Backup of the values segment of the sheet.
+		/// </summary>
+		public string ValuesCache { get; set; }
+		/// <summary>
+		/// Date of the last time the sheet was updated.
+		/// </summary>
+		public DateTime LastUpdated { get; set; }
 	}
 }
--- NethysBot/Services/SheetService.cs
+++ NethysBot/Services/SheetService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 using System.Net.Http;
@@ -8,6 +9,8 @@ using System.Text.RegularExpressions;
 using LiteDB;
 using System.Net;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using Discord.Commands;
 
 namespace NethysBot.Services
 {
@@ -16,12 +19,11 @@ namespace NethysBot.Services
 
 		private LiteCollection<Character> collection = Program.Database.GetCollection<Character>("Characters");
 		private HttpClient Client;
-		private string Url;
+		private string Api = "http://character.pf2.tools/api/characters/";
 		
 		public SheetService(IConfiguration config)
 		{
 			Client = new HttpClient();
-			Url = config["apiurl"];
 		}
 
 		/// <summary>
@@ -29,13 +31,13 @@ namespace NethysBot.Services
 		/// </summary>
 		/// <param name="url">Url of the character</param>
 		/// <returns>Parsed and updated character</returns>
-		public async Task<Character> GetCharacter(string url)
+		public async Task<Character> GetCharacter(string url, SocketCommandContext context = null)
 		{
-			var regex = new Regex(@"(\W*https?:\/\/\W*)?(\w*character\w*)\.(\w*pf2\w*)\.(\w*tools\w*)\/\?(\w{8})");
+			var regex = new Regex(@"(\w*\W*)?\?(\w*)\-?");
 
 			if (!regex.IsMatch(url))
 			{
-				throw new Exception("Invalid Url");
+				throw new Exception("This is not a valid character.pf2.tools url. Makesure you copy the full url!");
 			}
 
 			var match = regex.Match(url);
@@ -44,30 +46,92 @@ namespace NethysBot.Services
 
 			if (!collection.Exists(x => x.RemoteId == id))
 			{
-				collection.Insert(new Character()
+				var c = new Character()
 				{
 					RemoteId = id
-				});
+				};
+
+				if (context != null) c.Owner = context.User.Id;
+
+				collection.Insert(c);
 			}
 
 			Character character = collection.FindOne(x => x.RemoteId == id);
 
-			// TODO: API get request
-			// Process: 
-			// 1) Obtain JSON from API
-			// 2) Store JSON into Cache
-			// 3) Send the character for Updating
+			HttpResponseMessage response = await Client.GetAsync(Api + id);
 
+			response.EnsureSuccessStatusCode();
+
+			string responsebody = await response.Content.ReadAsStringAsync();
+
+			var json = JObject.Parse(responsebody);
+			
+			if (json.ContainsKey("error"))
+			{
+				throw new Exception("This is not a valid character ID or the character is not set to public.");
+			}
+
+			character.SheetCache = json["data"].ToString();
+
+			character.ValuesCache = json["values"].ToString();
+
+			character.LastUpdated = DateTime.Now;
+
+			character.Name = (string)json["name"];
+
+			collection.Update(character);
+
+			return character;
 		}
 
 		/// <summary>
-		/// Updates a character
+		/// Syncs a character with the remote version
 		/// </summary>
 		/// <param name="Character"></param>
-		/// <returns></returns>
-		public async Task<Character> ParseCharacter(Character Character)
+		/// <returns>The Synced character</returns>
+		public async Task<Character> SyncCharacter(Character Character)
 		{
-			// TODO: Parse JSON obtained from the API
+			HttpResponseMessage response = await Client.GetAsync(Api + Character.RemoteId);
+
+			response.EnsureSuccessStatusCode();
+
+			string responsebody = await response.Content.ReadAsStringAsync();
+
+			var json = JObject.Parse(responsebody);
+
+			Character.SheetCache = json["data"].ToString();
+
+			Character.ValuesCache = json["values"].ToString();
+
+			Character.LastUpdated = DateTime.Now;
+
+			Character.Name = (string)json["name"];
+
+			collection.Update(Character);
+
+			return Character;
+		}
+
+		/// <summary>
+		/// Syncs a character's calculated values with the remote version
+		/// </summary>
+		/// <param name="Character"></param>
+		/// <returns>The Synced character</returns>
+		public async Task<Character> SyncValues(Character Character)
+		{
+			HttpResponseMessage response = await Client.GetAsync(Api+ Character.RemoteId + "/values");
+
+			response.EnsureSuccessStatusCode();
+
+			string responsebody = await response.Content.ReadAsStringAsync();
+
+			var json = JObject.Parse(responsebody);
+
+			Character.ValuesCache = json["data"].ToString();
+
+			collection.Update(Character);
+
+			return Character;
 		}
 	}
 }
