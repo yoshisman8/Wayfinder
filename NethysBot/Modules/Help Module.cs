a7044e8d31b02fba2cf009d2b8fbe9228551518c
yoshisman8 <4214215+yoshisman8@users.noreply.github.com>
2020-01-30 16:11:31 -0400

    semicolons are hard

--- NethysBot/Modules/Help Module.cs
+++ NethysBot/Modules/Help Module.cs
@@ -74,7 +74,7 @@ namespace NethysBot.Modules
 						}
 					}
 
-					embed.AddField("Symbology","`[Argument]` means it is a mandatory argument.\n`<Argument>` means it's an optional argument.")
+					embed.AddField("Symbology", "`[Argument]` means it is a mandatory argument.\n`<Argument>` means it's an optional argument.");
 					var DMs = await Context.User.GetOrCreateDMChannelAsync();
 					if (Context.Guild != null)
 					{
